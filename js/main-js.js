6a2fa564bb243ce10b3e0944f17b86ddc1a0feff
esraa-ismail <esraa.m.ismail@gmail.com>
2021-02-04 07:47:07 +0200

    Esraa-Ismail: add Explore Egypt Page

--- js/main-js.js
+++ js/main-js.js
@@ -149,6 +149,40 @@ $('.slider-nav').slick({
   focusOnSelect: true
 });
 
+//hony moon
+$('.honymoon1').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3 ,
+  dots: true,
+  speed: 300,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1 } 
+      }
+     ]
+});
+
+
 
 
 
