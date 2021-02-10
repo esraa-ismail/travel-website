0e6b930621237676dc5750768b8a9fd87cc245dc
esraa-ismail <esraa.m.ismail@gmail.com>
2021-02-10 05:18:15 +0200

    Esraa-ismail: added blogs and single blog page

--- js/main-js.js
+++ js/main-js.js
@@ -183,7 +183,32 @@ $('.honymoon1').slick({
 });
 
 
-
+//aluxer 
+$('.al-auxer2').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
 
 
  //back-to-top
