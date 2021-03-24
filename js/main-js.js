ca1fab801ce761cd8e0a4e8ac57f5729cfbddbc3
esraa-ismail <esraa.m.ismail@gmail.com>
2021-03-24 18:27:00 +0200

    fixed error

--- js/main-js.js
+++ js/main-js.js
@@ -279,48 +279,6 @@ $('.al-auxer2').slick({
     ]
 });
 
-// Porfolio isotope and filter
-$(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-        $("#portfolio-flters li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-
-        portfolioIsotope.isotope({
-            filter: $(this).data('filter')
-        });
-        aos_init();
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-        $('.venobox').venobox();
-    });
-});
-
-// Portfolio details carousel
-$(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-});
-
-// Init AOS
-function aos_init() {
-    AOS.init({
-        duration: 1000,
-        easing: "ease-in-out",
-        once: true,
-        mirror: false
-    });
-}
-$(window).on('load', function() {
-    aos_init();
-});
 
 
 //back-to-top
